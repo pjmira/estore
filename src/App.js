bab2fc3f10b73fc5daf427a9b9092f5a24f5db72
Pooja Bhakta <pjmira19@gmail.com>
2023-08-21 15:08:24 -0400

    New line added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import CatNav from './Components/CatNav';
 import MainComponent from './Components/MainComponent';
 import TopNav from './Components/TopNav';
-
+// A new line added
 function App() {
   return (
     <div className="App">
